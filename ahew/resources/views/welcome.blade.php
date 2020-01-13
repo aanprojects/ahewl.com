7b9969472d600ce722dadac51a71624cf4a2e0f2
AAN <aalnoman@outlook.com>
2020-01-14 00:12:43 +0600

    Project setup

--- ahew/resources/views/welcome.blade.php
+++ ahew/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<!-- Take a moment -->
         <title>Laravel</title>
 
         <!-- Fonts -->
